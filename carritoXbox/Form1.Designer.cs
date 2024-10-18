b67d87850cfec868330ffc0d46ece6787d2e8b7c
JOSE EDUARDO GIJON <alu.21130883@correo.itlalaguna.edu.mx>
2024-10-18 10:38:01 -0600

    Agregar archivos de proyecto.

---
+++ carritoXbox/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace carritoXbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb_Puertos = new System.Windows.Forms.ComboBox();
+            this.btn_Conectar = new System.Windows.Forms.Button();
+            this.btn_Actualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cb_Puertos
+            // 
+            this.cb_Puertos.FormattingEnabled = true;
+            this.cb_Puertos.Location = new System.Drawing.Point(42, 31);
+            this.cb_Puertos.Name = "cb_Puertos";
+            this.cb_Puertos.Size = new System.Drawing.Size(121, 21);
+            this.cb_Puertos.TabIndex = 0;
+            // 
+            // btn_Conectar
+            // 
+            this.btn_Conectar.Location = new System.Drawing.Point(186, 28);
+            this.btn_Conectar.Name = "btn_Conectar";
+            this.btn_Conectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Conectar.TabIndex = 1;
+            this.btn_Conectar.Text = "CONECTAR";
+            this.btn_Conectar.UseVisualStyleBackColor = true;
+            this.btn_Conectar.Click += new System.EventHandler(this.btn_Conectar_Click);
+            // 
+            // btn_Actualizar
+            // 
+            this.btn_Actualizar.Location = new System.Drawing.Point(283, 28);
+            this.btn_Actualizar.Name = "btn_Actualizar";
+            this.btn_Actualizar.Size = new System.Drawing.Size(91, 23);
+            this.btn_Actualizar.TabIndex = 2;
+            this.btn_Actualizar.Text = "ACTUALIZAR";
+            this.btn_Actualizar.UseVisualStyleBackColor = true;
+            this.btn_Actualizar.Click += new System.EventHandler(this.btn_Actualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 234);
+            this.Controls.Add(this.btn_Actualizar);
+            this.Controls.Add(this.btn_Conectar);
+            this.Controls.Add(this.cb_Puertos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SUPER CARRITO BT";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_Puertos;
+        private System.Windows.Forms.Button btn_Conectar;
+        private System.Windows.Forms.Button btn_Actualizar;
+    }
+}
+
---
+++ carritoXbox/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.XInput;
+
+namespace carritoXbox
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort sp = new SerialPort();
+        Controller controller;
+        Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            controller = new Controller(UserIndex.One);
+            timer = new Timer();
+            timer.Interval = 100; // Intervalo de 100ms
+            timer.Tick += Timer_Tick;
+            if (!controller.IsConnected)
+            {
+                MessageBox.Show("El controlador no está conectado.");
+                return; // Sale si no está conectado
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+
+            if (puertos.Length == 0)
+            {
+                MessageBox.Show("No se detectaron puertos.");
+            }
+            else
+            {
+                cb_Puertos.DataSource = puertos;
+            }
+        }
+
+        private void btn_Conectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sp.PortName = cb_Puertos.Text;
+                sp.BaudRate = 9600; // Asegúrate de que la velocidad en baudios coincida con la del módulo Bluetooth
+                sp.Open();
+                MessageBox.Show("Conectado al puerto " + sp.PortName);
+                timer.Start(); // Iniciar el temporizador para leer el estado del controlador
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (controller.GetState(out var state) && sp.IsOpen)
+            {
+                var leftThumb = state.Gamepad.LeftThumbY;   // Joystick izquierdo para girar
+                var rightThumb = state.Gamepad.RightThumbX; // Joystick derecho para avanzar/retroceder
+
+                // Avanza si el joystick derecho está hacia adelante
+                if (rightThumb > 5000)
+                {
+                    sp.Write("w"); // Comando para avanzar
+                }
+                // Retrocede si el joystick derecho está hacia atrás
+                else if (rightThumb < -5000)
+                {
+                    sp.Write("s"); // Comando para retroceder
+                }
+                // Gira a la derecha si el joystick izquierdo está hacia la derecha
+                else if (leftThumb > 5000)
+                {
+                    sp.Write("d"); // Comando para girar a la derecha
+                }
+                // Gira a la izquierda si el joystick izquierdo está hacia la izquierda
+                else if (leftThumb < -5000)
+                {
+                    sp.Write("a"); // Comando para girar a la izquierda
+                }
+                // Si no hay movimiento, detener el carrito
+                else
+                {
+                    sp.Write("p"); // Comando para detenerse
+                }
+            }
+        }
+
+
+        private void btn_Actualizar_Click(object sender, EventArgs e)
+        {
+            string[] puertos = SerialPort.GetPortNames();
+
+            if (puertos.Length == 0)
+            {
+                MessageBox.Show("No se encontraron puertos disponibles.");
+            }
+            else
+            {
+                cb_Puertos.DataSource = puertos;
+            }
+        }
+    }
+}
